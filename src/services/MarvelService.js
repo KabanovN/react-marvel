dbad7dade6c8e97ac36c262f40715c51029da1b1
Nikita <kabanov.n.v@mail.ru>
2021-12-15 19:37:25 +0300

    доработка функциональности блока random character

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -12,13 +12,27 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    //метод для трансформации данных из полученного объекта - вычленяем нужные данные в новый объект
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 226)}...` : 'You can learn more information on Homepage or Wiki',
+            thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 
-    getCharacter = (id) => {
-        return this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getAllCharacters = async () => {
+        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
+
+    getCharacter = async (id) => {
+        const res = await this.getResources(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
 }
 
 export default MarvelService;
